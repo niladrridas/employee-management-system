1b1c1a3ef111fe5e95ac48f01ff3a45db4fc14bd
Niladri Das <niladri.das@lpu.in>
2024-01-17 02:43:39 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,10 @@
+package com.yourcompany.employee;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String position;
+    private double salary;
+
+    // Constructors, getters, and setters
+}
